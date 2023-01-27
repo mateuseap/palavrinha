ab55a1ef9709566f1334e217a2a5b9b168d38bb8
Mateus Elias <meap@cin.ufpe.br>
2023-01-27 01:22:19 -0300

    feat(Letter): getting the state of const 'board' through 'useContext'

--- src/components/Letter.js
+++ src/components/Letter.js
@@ -1,5 +1,10 @@
+import { useContext } from "react";
+import { AppContext } from "../App";
+
 function Letter({ letterPosition, attemptValue }) {
-  return <div className="letter">Letter</div>;
+  const { board } = useContext(AppContext);
+  const letter = board[attemptValue][letterPosition];
+  return <div className="letter">{letter}</div>;
 }
 
 export default Letter;
