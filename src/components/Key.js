4e9e33873303d1cdeccb76720a3c93951fe9d7ba
Mateus Elias <meap@cin.ufpe.br>
2023-01-27 03:20:13 -0300

    feat(Key): keys functional on click

--- src/components/Key.js
+++ src/components/Key.js
@@ -2,9 +2,42 @@ import { useContext } from "react";
 import { AppContext } from "../App";
 
 function Key({ keyValue, bigKey }) {
-  const { board } = useContext(AppContext);
+  const { board, setBoard, currentAttempt, setCurrentAttempt } =
+    useContext(AppContext);
+
+  const selectLetter = () => {
+    if (keyValue === "ENTER") {
+      if (currentAttempt.letterPosition !== 5) return;
+      setCurrentAttempt({
+        attempt: currentAttempt.attempt + 1,
+        letterPosition: 0,
+      });
+    } else if (keyValue === "DELETE") {
+      if (currentAttempt.letterPosition === 0) return;
+      const newBoard = [...board];
+      newBoard[currentAttempt.attempt][currentAttempt.letterPosition - 1] = "";
+      setBoard(newBoard);
+      setCurrentAttempt({
+        ...currentAttempt,
+        letterPosition: currentAttempt.letterPosition - 1,
+      });
+    } else {
+      if (currentAttempt.letterPosition > 4) return;
+
+      const newBoard = [...board];
+      newBoard[currentAttempt.attempt][currentAttempt.letterPosition] =
+        keyValue;
+
+      setBoard(newBoard);
+      setCurrentAttempt({
+        ...currentAttempt,
+        letterPosition: currentAttempt.letterPosition + 1,
+      });
+    }
+  };
+
   return (
-    <div className="key" id={bigKey && "big"}>
+    <div className="key" id={bigKey && "big"} onClick={selectLetter}>
       {keyValue}
     </div>
   );
