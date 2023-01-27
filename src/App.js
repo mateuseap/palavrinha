9ae4db88c60d0b9411a429079eed529c75c6ec87
Mateus Elias <meap@cin.ufpe.br>
2023-01-27 02:36:06 -0300

    feat(App): centralized 'Board' and 'Keyboard' components

--- src/App.js
+++ src/App.js
@@ -15,8 +15,10 @@ function App() {
         <h1>Palavrinha</h1>
       </nav>
       <AppContext.Provider value={{ board, setBoard }}>
-        <Board />
-        <Keyboard />
+        <div className="game">
+          <Board />
+          <Keyboard />
+        </div>
       </AppContext.Provider>
     </div>
   );
