669d82a7161c16ec5276f1b450110747aa819c69
Mateus Elias <meap@cin.ufpe.br>
2023-01-27 02:49:51 -0300

    feat(Keyboard): all keys set

--- src/components/Keyboard.js
+++ src/components/Keyboard.js
@@ -1,5 +1,31 @@
+import Key from "./Key";
+
 function Keyboard() {
-  return <div>Keyboard</div>;
+  const keys1 = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"];
+  const keys2 = ["A", "S", "D", "F", "G", "H", "J", "K", "L"];
+  const keys3 = ["Z", "X", "C", "V", "B", "N", "M"];
+
+  return (
+    <div className="keyboard">
+      <div className="line1">
+        {keys1.map((key) => {
+          return <Key keyValue={key} />;
+        })}
+      </div>
+      <div className="line2">
+        {keys2.map((key) => {
+          return <Key keyValue={key} />;
+        })}
+      </div>
+      <div className="line3">
+        <Key keyValue={"ENTER"} bigKey={true} />
+        {keys3.map((key) => {
+          return <Key keyValue={key} />;
+        })}
+        <Key keyValue={"DELETE"} bigKey={true} />
+      </div>
+    </div>
+  );
 }
 
 export default Keyboard;
