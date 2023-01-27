5f3beba4aeb032405f00103deb5531e464206952
Mateus Elias <meap@cin.ufpe.br>
2023-01-27 01:06:10 -0300

    feat(Board): structured the base of the board

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,5 +1,55 @@
+import { useState } from "react";
+import { boardDefault } from "../utils/Words";
+import Letter from "./Letter";
+
 function Board() {
-  return <div>Board</div>;
+  const [board, setBoard] = useState(boardDefault);
+  return (
+    <div className="board">
+      <div className="row">
+        <Letter letterPosition={0} attemptValue={0} />
+        <Letter letterPosition={1} attemptValue={0} />
+        <Letter letterPosition={2} attemptValue={0} />
+        <Letter letterPosition={3} attemptValue={0} />
+        <Letter letterPosition={4} attemptValue={0} />
+      </div>
+      <div className="row">
+        <Letter letterPosition={0} attemptValue={1} />
+        <Letter letterPosition={1} attemptValue={1} />
+        <Letter letterPosition={2} attemptValue={1} />
+        <Letter letterPosition={3} attemptValue={1} />
+        <Letter letterPosition={4} attemptValue={1} />
+      </div>
+      <div className="row">
+        <Letter letterPosition={0} attemptValue={2} />
+        <Letter letterPosition={1} attemptValue={2} />
+        <Letter letterPosition={2} attemptValue={2} />
+        <Letter letterPosition={3} attemptValue={2} />
+        <Letter letterPosition={4} attemptValue={2} />
+      </div>
+      <div className="row">
+        <Letter letterPosition={0} attemptValue={3} />
+        <Letter letterPosition={1} attemptValue={3} />
+        <Letter letterPosition={2} attemptValue={3} />
+        <Letter letterPosition={3} attemptValue={3} />
+        <Letter letterPosition={4} attemptValue={3} />
+      </div>
+      <div className="row">
+        <Letter letterPosition={0} attemptValue={4} />
+        <Letter letterPosition={1} attemptValue={4} />
+        <Letter letterPosition={2} attemptValue={4} />
+        <Letter letterPosition={3} attemptValue={4} />
+        <Letter letterPosition={4} attemptValue={4} />
+      </div>
+      <div className="row">
+        <Letter letterPosition={0} attemptValue={5} />
+        <Letter letterPosition={1} attemptValue={5} />
+        <Letter letterPosition={2} attemptValue={5} />
+        <Letter letterPosition={3} attemptValue={5} />
+        <Letter letterPosition={4} attemptValue={5} />
+      </div>
+    </div>
+  );
 }
 
 export default Board;
