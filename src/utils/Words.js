973d7e92c2c0e6cbe2444f23b49a16d5bfedc467
Mateus Elias <meap@cin.ufpe.br>
2023-01-28 15:43:29 -0300

    feat(Words): 'generateWordSet' function implemented

--- src/utils/Words.js
+++ src/utils/Words.js
@@ -1,3 +1,5 @@
+import wordBank from "../words/all-words.txt";
+
 export const boardDefault = [
   ["", "", "", "", ""],
   ["", "", "", "", ""],
@@ -6,3 +8,15 @@ export const boardDefault = [
   ["", "", "", "", ""],
   ["", "", "", "", ""],
 ];
+
+export const generateWordSet = async () => {
+  let wordSet;
+  await fetch(wordBank)
+    .then((response) => response.text())
+    .then((result) => {
+      const wordArray = result.split("\n");
+      wordSet = new Set(wordArray);
+    });
+
+  return { wordSet };
+};
