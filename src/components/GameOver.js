50016c21da70b601bbc21650452cf5dd38f7a5ee
Mateus Elias <meap@cin.ufpe.br>
2023-01-28 16:19:59 -0300

    feat(GameOver): component fully implemented

--- src/components/GameOver.js
+++ src/components/GameOver.js
@@ -1,5 +1,21 @@
+import { useContext } from "react";
+import { AppContext } from "../App";
+
 function GameOver() {
-  return <div className="gameOver">Game Over</div>;
+  const { currentAttempt, correctWord, gameOver } = useContext(AppContext);
+  return (
+    <div className="gameOver">
+      <h3>
+        {gameOver.guessedWord
+          ? "Parabéns, você acertou a palavra!"
+          : "Que pena, você perdeu :("}
+      </h3>
+      <h1>Resposta: {correctWord}</h1>
+      {gameOver.guessedWord && (
+        <h3>Você acertou na {currentAttempt.attemptValue}ª tentativa!</h3>
+      )}
+    </div>
+  );
 }
 
 export default GameOver;
