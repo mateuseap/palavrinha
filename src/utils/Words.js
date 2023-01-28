6d404fd4f6f29409e4d5a4882dd684aef4324fa9
Mateus Elias <meap@cin.ufpe.br>
2023-01-28 16:22:35 -0300

    feat(Word): getting a random word from 'wordArray'

--- src/utils/Words.js
+++ src/utils/Words.js
@@ -11,12 +11,14 @@ export const boardDefault = [
 
 export const generateWordSet = async () => {
   let wordSet;
+  let todaysWord;
   await fetch(wordBank)
     .then((response) => response.text())
     .then((result) => {
       const wordArray = result.split("\n");
+      todaysWord = wordArray[Math.floor(Math.random() * wordArray.length)]
       wordSet = new Set(wordArray);
     });
 
-  return { wordSet };
+  return { wordSet, todaysWord };
 };
