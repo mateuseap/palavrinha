388569130529ce55c558df94aa5c9cc0c18d134e
Mateus Elias <meap@cin.ufpe.br>
2023-01-28 15:48:39 -0300

    feat(Letter): setting letters color according to the correct word

--- src/components/Letter.js
+++ src/components/Letter.js
@@ -2,9 +2,21 @@ import { useContext } from "react";
 import { AppContext } from "../App";
 
 function Letter({ letterPosition, attemptValue }) {
-  const { board } = useContext(AppContext);
+  const { board, currentAttempt, correctWord } = useContext(AppContext);
   const letter = board[attemptValue][letterPosition];
-  return <div className="letter">{letter}</div>;
+
+  const correct = correctWord[letterPosition] === letter;
+  const almost = !correct && letter !== "" && correctWord.includes(letter);
+
+  const letterState =
+    currentAttempt.attemptValue > attemptValue ?
+    (correct ? "correct" : almost ? "almost" : "error") : "default";
+
+  return (
+    <div className="letter" id={letterState}>
+      {letter}
+    </div>
+  );
 }
 
 export default Letter;
